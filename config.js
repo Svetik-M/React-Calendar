61c6a5f5a8147c507926ba2540bfd8e533ea3c47
Svetlana Morozko <morozko.svetlana@gmail.com>
2016-09-30 00:00:06 +0300

    Change DB config

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ var config = {
     connectDB: function() {
         var pgp = require("pg-promise")();
         var connection = {
-            host: 'localhost',
+            host: 'ec2-184-73-202-229.compute-1.amazonaws.com',
             port: 5432,
-            database: 'DB',
-            user: 'postgres',
-            password: '8235648'
+            database: 'd8uaa32fcj80a7',
+            user: 'tprnprooysdlcg',
+            password: 'pdTR6NAXEE36wuQ6ds3-xC_EZo'
         };
         return pgp(connection);
     }
