b8a8314de987dec11ee6147d622aa1491c05d983
Svetlana Morozko <morozko.svetlana@gmail.com>
2016-10-26 22:00:14 +0300

    Fix styles

--- config.js
+++ config.js
@@ -14,4 +14,18 @@ var config = {
     }
 };
 
+// var config = {
+//     connectDB: function() {
+//         var pgp = require("pg-promise")();
+//         var connection = {
+//             host: 'localhost',
+//             port: 5432,
+//             database: 'DB',
+//             user: 'postgres',
+//             password: '8235648'
+//         };
+//         return pgp(connection);
+//     }
+// };
+
 module.exports = config;
